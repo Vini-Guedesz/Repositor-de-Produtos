a49ee65e2c4c6211ddeaa24d5be49a30e366bacf
Vini-Guedesz <marcosviniciosguedesdossantos@gmail.com>
2023-08-15 23:49:28 -0300

    Correção na função "cadastrar"

--- repositor de produtos.py
+++ repositor de produtos.py
@@ -6,10 +6,20 @@ lista_produtos = [{'produto' : 'heiniken', 'quantidade' : 4, 'preço' : 23 },
 
 def cadastrar():
 
-    limpar()
+    while True:
+        produto = input('nome do produto: ').lower()
+        produto_existente = False  
+        for dicionario in lista_produtos:
+            if dicionario['produto'] == produto:
+                carregar()
+                print('produto já existente, digite outro nome!!!')
+                produto_existente = True
+                break  
+        if not produto_existente:
+            break  
 
-    produto = input('nome do produto: ').lower()
 
+            
     while True:
         preço = input('preço do produto: ').replace(',' , '')
         if preço.isdigit() and float(preço) > 0:
@@ -34,19 +44,40 @@ def cadastrar():
 
     print('produto registrado com sucesso!!!')
     carregar()
-    limpar()
 
 
 
 
 def listar():
-    print()
+
     print('---------------------')
     lista_produtos_ordenados = sorted(lista_produtos, key=lambda x: x['produto'].lower())
     for produto in lista_produtos_ordenados:
         for chave, valor in produto.items():
                 print(f"  {chave.capitalize()}: {valor}")
         print('---------------------')
+
+    print("[1] Alterar Produto")
+    print("[2] Remover Produto")
+    print("[0] Voltar ao Menu")
+    while True:
+        opção = input('Escolha sua opção:')
+        if not opção.isdigit():
+            print('digite um valor valido!!!')
+        else:
+            match int(opção):
+                case 1:
+                    carregar()
+                    alterar()
+                case 2:
+                    carregar()
+                    remover()
+                case 0:
+                    carregar()
+                    break
+                case _:
+                    print('digite um valor valido!!!')
+    
 
 
 
@@ -73,7 +104,7 @@ def pesquisar():
 
 
 def alterar():
-    limpar()
+
     print('======================')
     print('Alteração de Produto')
     print('======================')
@@ -130,10 +161,11 @@ def alterar():
                         else:
                             print('digite um valor valido!!!')
 
-    if int(produto) == 0:
+    if produto == '0':
         carregar()
-        limpar()            
+        menu()            
     else:
+        carregar()
         print('Produto não encontrado!!!')
         alterar()
 
@@ -169,11 +201,14 @@ def carregar():
     for _ in range(5):
         print(".", end="", flush=True)
         time.sleep(0.5)
+    os.system('cls') or None
+
 
 
 
 
 def menu():
+        limpar()
         print('======================')
         print("[1] Cadastrar Produto")
         print("[2] Listar Produtos")
@@ -182,6 +217,7 @@ def menu():
         print("[5] Remover Produto")
         print("[0] Sair do programa")
         print('======================')
+
 
 
 
@@ -195,14 +231,19 @@ while True:
         opção = int(opção)
         match opção:    
             case 1:
+                limpar()
                 cadastrar()
             case 2:
+                limpar()
                 listar()
             case 3:
+                limpar()
                 pesquisar()
             case 4:
+                limpar()
                 alterar()
             case 5:
+                limpar()
                 remover()
             case 0:
                 break
